9f05ac19b5d14e895143688af83b4df16d54eeaa
ericheep <ericheep@gmail.com>
2017-05-08 01:25:45 +0000

    chchchchanges

--- python/ultrasonic-test.py
+++ python/ultrasonic-test.py
@@ -16,21 +16,21 @@ GPIO.output(TRIG, False)
 print "Waiting for Sensor to Settle"
 time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+while (True):
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
+	while GPIO.input(ECHO)==0:
+	    pulse_start = time.time()
 
-while GPIO.input(ECHO)==1:
-    pulse_end = time.time()
+	while GPIO.input(ECHO)==1:
+	    pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start
-distance = pulse_duration * 17150
-distance = round(distance, 2)
-print "Distance:", distance, "cm"
+	pulse_duration = pulse_end - pulse_start
+	distance = pulse_duration * 17150
+	distance = round(distance, 2)
+	print "Distance:", distance, "cm"
+	time.sleep(0.25)
 
 GPIO.cleanup()
-
-
